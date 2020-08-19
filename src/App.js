dea96aee64122c7e16adee7a298b9eab90681e36
Michele Ambrosio <michele.ambrosio@eduzz.com>
2020-08-19 11:29:37 -0300

    List, add and delete repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await api.get('/repositories'); 
+        setRepositories(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+  
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post('/repositories', {
+        title: `Novo Projeto ${Date.now()}`,
+        url: 'https://github.com/rocketseat-education/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs',
+        techs: ['NodeJS', 'ReactJS']
+      });
+
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+
+      const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+      
+      if (repositoryIndex < 0) {
+        return;
+      }
+    
+      const newRepositories = [...repositories];
+      newRepositories.splice(repositoryIndex, 1);
+
+      setRepositories(newRepositories);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
